0a8fff27f529edb8d12274c0312ab3095dc7f62d
Miroslav <hgmirko@gmail.com>
2021-02-27 00:22:38 +0100

    Add files via upload

--- maja_torte/src/components/PojedinacnaKategorija.vue
+++ maja_torte/src/components/PojedinacnaKategorija.vue
@@ -1,52 +1,51 @@
 <template>
-    <div class="pojedinacni">
-        <h2>{{type[0].kat_naziv}}</h2>
-            <div class="image" v-for="proizvod in proizvodi" :key="proizvod.pro_id">
-                <router-link  class="links" v-bind:to="'/Details/' +
-                 proizvod.pro_id"   >   
-                        <img :src="'data:image/jpg;base64,' + proizvod.pro_img" 
-                         alt="something went wrong!" />   
-                <h4>{{proizvod.pro_naziv}}</h4>
-                </router-link>
-            </div>
+  <div class="pojedinacni">
+    <div class="proizvod">
+      <router-link
+        v-bind:to="'/Proizvod/' + proizvod.pro_id"
+        v-for="proizvod in proizvodi"
+        :key="proizvod.pro_id"
+      >
+        <img class="img" :src="'data:image/jpg;base64,' + proizvod.pro_img" />
+        {{ proizvod.pro_naziv }}
+        {{ proizvod.pro_opis }}
+        {{ "CENA: " + proizvod.pro_cena + "din./kg" }}
+      </router-link>
     </div>
+    <!-- <button @click="poruci">Poručite proizvod</button> -->
+  </div>
 </template>
 <script>
-import axios from 'axios';
+import axios from "axios";
 export default {
-    data(){
-        return{
-            proizvodi:[],
-            type: []
-        }
+  data() {
+    return {
+      proizvodi: [],
+    };
+  },
+  methods: {
+    getProizvodi() {
+      axios
+        .get("http://231j122.mars1.mars-hosting.com/api/proizvodi", {
+          params: { kat_id: this.$route.params.id },
+        })
+        .then((res) => {
+          console.log(res.data.data);
+          this.proizvodi = res.data.data;
+        });
     },
-    methods:{
-        getProizvodi(){
-            axios.get('http://231j122.mars1.mars-hosting.com/api/proizvodi',{
-                params:{kat_id:this.$route.params.id}
-            }).then((res)=>{
-                console.log(res.data.data);
-                this.proizvodi=res.data.data;
-                console.log(res.data.type);
-                this.type= res.data.type;
-            })
-        },
-   
-    },
-    
-     mounted(){
-    this.getProizvodi();
+    poruci(){
+        this.$router.push('/Porudzbenica')
     }
-}
+  },
+  mounted() {
+    this.getProizvodi();
+  },
+};
 </script>
 <style scoped>
-img{
-    width: 400px;
-    height: auto;
-}
-.image{
-    display: grid;
-    grid-template-rows: 4fr 1fr;
-    justify-items: center;
+img {
+  width: 250px;
+  height: 250px;
 }
 </style>
