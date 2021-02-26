624a84b984d5282da7f37c0ae63d234aa48048ec
hgmirko@gmail.com <hgmirko@gmail.com>
2021-02-26 22:05:51 +0100

    login
    logout

--- maja_torte/src/components/Details.vue
+++ maja_torte/src/components/Details.vue
@@ -11,7 +11,7 @@ export default {
         return{
           product: [],
           picture: [],
-          proizvod_id: ''
+          proizvod_id: window.location.pathname
         }
     },
     methods: {
--- maja_torte/src/router/index.js
+++ maja_torte/src/router/index.js
@@ -20,7 +20,7 @@ const routes = [
     component: SignUp
   },
   {
-    path:'/Details',
+    path:'/Details/:pro_id',
     name:'Details',
     component: Details
   },
